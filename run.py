bc3086f18c808e4e92b88b252b54f038bf25dba3
Nicolas Emiliani <nicolas.emiliani@usmediaconsulting.com>
2013-10-11 17:40:00 -0300

    Disable garbage collection.

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-
+import gc
 from exchange import Exchange
 import logging
 
 from settings import ENDPOINT_LIST, EVENT_ENDPOINT, BALANCE_TO, LOG_LEVEL
 
 if __name__ == '__main__':
-
+    gc.disable()
     logging.basicConfig(
             level=LOG_LEVEL, 
             format='%(asctime)-15s %(levelname)s %(message)s')

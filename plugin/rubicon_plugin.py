b725f178b4ed1926e10efcb3db7981fd95e59dc7
Nicolas Emiliani <nicolas@data01>
2014-03-26 20:17:03 +0000

    Rubicon plugs updates

--- plugin/rubicon_plugin.py
+++ plugin/rubicon_plugin.py
@@ -5,32 +5,33 @@ import json
 import os
 import datetime
 
-WIN_PROBABILITY   = 3
-CLICK_PROBABILITY = 1
+WIN_PROBABILITY   = 5
+CLICK_PROBABILITY = 10
 
 BASE_PATH = 'plugin/rubicon_test'
 
 REQUEST_FILES = [
-    'rubicon_banner01_nw.json',
-    'rubicon_banner02_nw.json',
-    'rubicon_banner03_nw.json',
-    'rubicon_banner04_nw.json',
-    'rubicon_banner05_nw.json',
-    'rubicon_banner06_nw.json',
-    'rubicon_banner07_nw.json',
-    'rubicon_banner08_nw.json',
-    'rubicon_banner09_nw.json',
-    'rubicon_banner10_nw.json',
-    'rubicon_banner11_nw.json',
-    'rubicon_banner12_nw.json',
-    'rubicon_banner13_nw.json',
-    'rubicon_banner14_nw.json',
-    'rubicon_banner15_nw.json',
-    'rubicon_banner16_nw.json',
-    'rubicon_banner17_nw.json',
-    'rubicon_banner18_nw.json',
-    'rubicon_banner19_nw.json',
-    'rubicon_banner20_nw.json',
+    'rubicon_banner01_nw.mobile.json',
+#   'rubicon_banner01_nw.json',
+#   'rubicon_banner02_nw.json',
+#   'rubicon_banner03_nw.json',
+#   'rubicon_banner04_nw.json',
+#   'rubicon_banner05_nw.json',
+#   'rubicon_banner06_nw.json',
+#   'rubicon_banner07_nw.json',
+#   'rubicon_banner08_nw.json',
+#   'rubicon_banner09_nw.json',
+#   'rubicon_banner10_nw.json',
+#   'rubicon_banner11_nw.json',
+#    'rubicon_banner12_nw.json',
+#    'rubicon_banner13_nw.json',
+#    'rubicon_banner14_nw.json',
+#    'rubicon_banner15_nw.json',
+#    'rubicon_banner16_nw.json',
+#    'rubicon_banner17_nw.json',
+#    'rubicon_banner18_nw.json',
+#    'rubicon_banner19_nw.json',
+#    'rubicon_banner20_nw.json',
 ]
 
 
@@ -54,7 +55,7 @@ class RubiconPlugin(ParameterPlugin):
 
     def get_request(self):
         # create the request line
-        req_line = 'POST /auctions HTTP/1.1'
+        req_line = 'POST /rubicon HTTP/1.1'
         # set the headers
         headers = {}
         headers['Host'] = 'localhost'         
@@ -80,6 +81,7 @@ class RubiconPlugin(ParameterPlugin):
         # is it a bid ?
         if status_code == 204 :
             return (False, '', {}, '')
+        #return (False, '', {}, '')
         # throw the dice to see if it's a winner                
         win = random.randint(0, 9) < WIN_PROBABILITY
         if not win:
@@ -87,32 +89,57 @@ class RubiconPlugin(ParameterPlugin):
         js = json.loads(body)
         aid = js["seatbid"][0]["bid"][0]["id"][:-2]
         pcid = js["seatbid"][0]["bid"][0]["crid"]
+        adm = js["seatbid"][0]["bid"][0]["adm"]
+        logging.debug('plugin.rubicon : adm %s' % adm)
+        #adm = adm.translate(None,'\\')
+        logging.debug('plugin.rubicon : translation %s' % adm)
+        idx = adm.find('http://10.0.2.11:12340/impression/rubicon')
+        url = ''
+        for i in range(idx, len(adm)):
+            if adm[i] != '"':
+                url += adm[i]
+            else:
+                break
+        url = url.replace('${AUCTION_ID}', aid)
+        url = url.replace('${AUCTION_PRICE:BF}', 'A8026991338B87A4')
+        path = url.replace('http://10.0.2.11:12340','')
+        logging.debug('plugin.rubicon : url translation %s' % path)
         # we won, we need to return True with all
         # all the data used to construct the win
         # notification
-        #logging.debug('plugin.rubicon : sending win for %s' % aid)
-        #req_line = 'POST /win?ev=imp&auctionPrice=546CF33989B0EF0F' \
-        #            '&publisherCreativeId=abc&auctionId=%s HTTP/1.1' % aid
-        req_line = 'GET /trackImpression/%s/auctionId/%s/auctionPrice/546CF33989B0EF0F?externalSystemId=hola HTTP/1.1' % (pcid, aid)
+        logging.debug('plugin.rubicon : sending win for %s' % aid)
+        #req_line = 'GET /events?ev=imp&apr=03903E5BECC5F1E4' \
+        #            '&aid=%s HTTP/1.1' % aid
+        req_line = 'GET %s HTTP/1.1' % path
         headers = {}
         headers['Host'] = 'localhost'
         headers['Content-Type'] = 'application/json'
         delta = (datetime.datetime.now() - \
                     datetime.datetime(1970,1,1)).total_seconds()
-        body = WIN_REQ % (aid, str(delta))
-        headers['Content-Length'] = str(len(body))
+        #body = WIN_REQ % (aid, str(delta))
+        #body = ''
+        #headers['Content-Length'] = str(len(body))
         heads = self.headers_to_str(headers)
-        buf = '%s\r\n%s\r\n%s' % (req_line, heads, body)
+        buf = '%s\r\n%s\r\n' % (req_line, heads)
         # send the impression event in 0.5 secs
         self.adserver.send_event(buf, 0.5)
+        #return (False, '', {}, '')
         # roll the dice to see if we should click
         click = random.randint(0, 100) < CLICK_PROBABILITY
         if not click:
             return (False, '', {}, '')
-        #click_req_line = 'POST /click?ev=click' \
-        #            '&publisherCreativeId=abc&auctionId=%s HTTP/1.1' % aid
-        click_req_line = 'GET /clicktracking/chupala/connector/%s?auctionId=%s&externalSystemId=hola&url=http://www.sis.com HTTP/1.1' % (pcid, aid)
-        buf_click = '%s\r\n%s\r\n%s' % (click_req_line, heads, body)
+        idx = adm.find('http://10.0.2.11:12340/click/rubicon')
+        url = ''
+        for i in range(idx, len(adm)):
+            if adm[i] != '"':
+                url += adm[i]
+            else:
+                break
+        url = url.replace('${AUCTION_ID}', aid)
+        path = url.replace('http://10.0.2.11:12340','')
+        click_req_line = 'GET %s HTTP/1.1' % path
+        #click_req_line = 'GET /events?ev=cli&aid=%s HTTP/1.1' % aid
+        buf_click = '%s\r\n%s\r\n' % (click_req_line, heads)
         self.adserver.send_event(buf_click, 1.8)
         return (False, '', {}, '')
 
